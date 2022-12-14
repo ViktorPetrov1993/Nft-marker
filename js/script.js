d69ecae28e1a881a209ec8249054cabd7a2f7d18
viktor <viktorpetrov1993s@gmail.com>
2022-12-14 12:42:17 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// let denis2 = ['denis', 2, 'j', 7]
+// let denis = {
+//     // let = obyavlenie peremennoi ona hranit obekt (vse chto v figyrnih skobkah)asociativniy masiv
+//     name: 'denis', 
+//     surname: 1,
+//     denis3: denis2
+// }
+// console.log(denis2[0])
+// // stroka chislo massiv obekt - tipi dannih
+
+// for (let i = 0; i < denis2.length; i++) {
+//     console.log('cicle', denis2[i]) 
+// }
+
+// function test(x, y) {
+//     if (!x && !y) {
+//         alert('idi nahui')
+//     } else {
+//         console.log(x+y)
+//     }
+// }
+
+// test()
+
+// window.onload = function () {
+//     const menu = document.getElementsByClassName('leftblock_content')
+//     console.log(menu[0].children[1])
+// }
